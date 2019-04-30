d1fffab549b01a091ff30569ae8312ad321dadcd
Megan Carroll <46762397+scrambledmeggz@users.noreply.github.com>
2019-04-29 20:59:42 -0600

    Add files via upload

---
+++ site_info.py
@@ -0,0 +1,33 @@
+import json
+
+## define the key/value pairs for attributes associated with locations
+
+site_schema = {
+    "name": "",
+    "address1": "",
+    "address2": "",
+    "city" : "",
+    "state":"",
+    "postal code" : "",
+    "longitude": "",
+    "latitude" : "",
+    "phone" : "",
+    "type" : "",
+    "url" : "",
+    "isShelter":"",
+    "isIntake":"",
+    "hasMeals":"",
+    "isFood":"",
+    "isFamily":"",
+    "isPublic":"",
+    "isPrivate":"",
+    "isChildren":"",
+    "isWomen":"",
+    "isMen":"",
+    "isDayCenter":"",
+    "otherLimits":"",
+    "hours": "",
+    "otherServices": "",
+    "notes":"",
+    "services": ['service_name_here']
+}
