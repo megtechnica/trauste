4f5b7188ad88100c48674a572f9ebd333756bfed
Megan Carroll <46762397+scrambledmeggz@users.noreply.github.com>
2019-05-02 23:20:39 -0600

    Add files via upload

--- script to reduce zip_codes.py
+++ script to reduce zip_codes.py
@@ -1,17 +1,16 @@
 import json
 
-with open('CO_Zip_Codes.json', 'r') as zip_codes_json:
-    raw_data = json.load(zip_codes_json)
+with open('CO_Zip_Codes2.json', 'r') as zip_codes_json2:
+    raw_data = json.load(zip_codes_json2)
     for i in raw_data:
-        print(raw_data[i])
         fields = i["fields"]
         geometry = i["geometry"]
         current_record = { "city":fields["city"],
                             "zip":fields["zip"],
                             "coordinates":geometry["coordinates"]
         }
-        with open("CO_Zip_Codes2.json","a") as zip_codes_json2:
-            json.dumps(current_record, zip_codes_json2)
+        with open("CO_Zip_Codes.json","a") as zip_codes_json:
+            json.dump(current_record, zip_codes_json)
 
 
         
